5a03925bbaa604de5a8c99b7b5913757286e148f
Luke Kadima <146091079+Luckadima@users.noreply.github.com>
2025-08-19 17:00:04 +0200

    Add files via upload

---
+++ FirebaseConfig.js
@@ -0,0 +1,17 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAyTuSRW9VgHgLTaeErUm06JL63i2Wn1eU",
+//   authDomain: "projectutility.firebaseapp.com",
+//   projectId: "projectutility",
+//   storageBucket: "projectutility.firebasestorage.app",
+//   messagingSenderId: "454601325151",
+//   appId: "1:454601325151:web:88f1db67060fd5c6d1f9ea"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
